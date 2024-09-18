3a233900fdc95cac212a8b9ae07a31e5b1fe3bdd
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-18 16:57:02 +0900

    add spam

--- docs/database.js
+++ docs/database.js
@@ -495,5 +495,6 @@ var spam_items = [
   {"screen_name": "ColleenEll70005", "added_date": "2024-09-17T21:03:00+09:00", "frozen_date": "2100-01-01T00:00:00"},
   {"screen_name": "Gamze1064411", "added_date": "2024-09-17T21:03:00+09:00", "frozen_date": "2100-01-01T00:00:00"},
   {"screen_name": "AdrianaLuc12693", "added_date": "2024-09-18T11:40:00+09:00", "frozen_date": "2100-01-01T00:00:00"},
-  {"screen_name": "AshleyTyrr99604", "added_date": "2024-09-18T16:45:00+09:00", "frozen_date": "2100-01-01T00:00:00"}
+  {"screen_name": "AshleyTyrr99604", "added_date": "2024-09-18T16:45:00+09:00", "frozen_date": "2100-01-01T00:00:00"},
+  {"screen_name": "AshleyLowe39397", "added_date": "2024-09-18T16:51:00+09:00", "frozen_date": "2100-01-01T00:00:00"}
 ]
