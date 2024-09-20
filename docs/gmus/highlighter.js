20ea24de3f47317c5d4812d01c23729c38e20dc4
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-21 01:20:18 +0900

    update gmus

--- docs/gmus/highlighter.js
+++ docs/gmus/highlighter.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name        Like Spam Highlight
+// @namespace   https://github.com/shapoco/likespam
 // @match       https://x.com/*
 // @grant       none
 // @version     1.0
 // @author      Shapoco
 // @description いいねスパムリストに収録済みのユーザーに「収録済」のタグを表示します
 // @require     https://shapoco.github.io/likespam/gmus/db.js
+// @updateURL   https://shapoco.github.io/likespam/gmus/highlighter.js
+// @downloadURL https://shapoco.github.io/likespam/gmus/highlighter.js
+// @supportURL  https://shapoco.github.io/likespam
 // ==/UserScript==
 
 const spamRegex = new RegExp('^@(' + spamScreenNames.join('|') + ')$');
