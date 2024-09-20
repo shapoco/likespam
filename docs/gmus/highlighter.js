d26be4744f3b86227aa16313947405cae6380ea8
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-21 01:03:59 +0900

    add highlighter.js

---
+++ docs/gmus/highlighter.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Like Spam Highlight
+// @match       https://x.com/*
+// @grant       none
+// @version     1.0
+// @author      Shapoco
+// @description いいねスパムリストに収録済みのユーザーに「収録済」のタグを表示します
+// @require     https://shapoco.github.io/likespam/spam_screen_names.js
+// ==/UserScript==
+
+const spamRegex = new RegExp('^@(' + spamScreenNames.join('|') + ')$');
+
+setTimeout(scanSpams, 1000);
+
+function scanSpams() {
+  console.log('SCROLL');
+  const spans = document.getElementsByTagName('span');
+  const n = spans.length;
+  for(var i = 0; i < n; i++) {
+    const span = spans[i];
+    if (span.innerHTML.startsWith('@') && span.innerHTML.match(spamRegex)) {
+      span.innerHTML += ' <span style="background: #e00; color: white; border-radius: 999px; font-weight: bold;">&nbsp;収録済&nbsp;</span>';
+    }
+  }
+  setTimeout(scanSpams, 1000);
+}
