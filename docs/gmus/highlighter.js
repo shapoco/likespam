978d2e2b1b10c9f38988f32d72a9f4938cf9d501
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-24 08:03:38 +0900

    update DB

--- docs/gmus/highlighter.js
+++ docs/gmus/highlighter.js
@@ -3,10 +3,10 @@
 // @namespace   https://github.com/shapoco/likespam
 // @match       https://x.com/*
 // @grant       none
-// @version     1.0.134
+// @version     1.0.135
 // @author      Shapoco
 // @description いいねスパムリストに収録済みのユーザーに「収録済」のタグを表示します
-// @require     https://shapoco.github.io/likespam/gmus/db.js?20240924075924
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20240924080337
 // @updateURL   https://shapoco.github.io/likespam/gmus/highlighter.js
 // @downloadURL https://shapoco.github.io/likespam/gmus/highlighter.js
 // @supportURL  https://shapoco.github.io/likespam
@@ -17,7 +17,6 @@ const spamRegex = new RegExp('^@(' + spamScreenNames.join('|') + ')$');
 setTimeout(scanSpams, 1000);
 
 function scanSpams() {
-  console.log('SCROLL');
   const spans = document.getElementsByTagName('span');
   const n = spans.length;
   for(var i = 0; i < n; i++) {
