b633f14213265db0159e315ebd6509cc4c1729c5
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-23 18:21:27 +0900

    fix description

--- docs/gmus/highlighter.js
+++ docs/gmus/highlighter.js
@@ -3,10 +3,10 @@
 // @namespace   https://github.com/shapoco/likespam
 // @match       https://x.com/*
 // @grant       none
-// @version     1.0.94
+// @version     1.0.95
 // @author      Shapoco
 // @description いいねスパムリストに収録済みのユーザーに「収録済」のタグを表示します
-// @require     https://shapoco.github.io/likespam/gmus/db.js?20240923175910
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20240923182118
 // @updateURL   https://shapoco.github.io/likespam/gmus/highlighter.js
 // @downloadURL https://shapoco.github.io/likespam/gmus/highlighter.js
 // @supportURL  https://shapoco.github.io/likespam
