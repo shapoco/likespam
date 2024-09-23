7b9f3d8b74f4c90fb65c7b6d442a307d06b671ab
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-24 08:38:52 +0900

    update DB

---
+++ docs/gmus/inforeader.js
@@ -0,0 +1,244 @@
+// ==UserScript==
+// @name        Spam Info Reader
+// @namespace   https://github.com/shapoco/likespam
+// @match       https://x.com/search?*
+// @grant       none
+// @version     1.0.2
+// @author      Shapoco
+// @description Xの検索結果からスパムアカウントの情報を抽出します
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20240924083852
+// @updateURL   https://shapoco.github.io/likespam/gmus/inforeader.js
+// @downloadURL https://shapoco.github.io/likespam/gmus/inforeader.js
+// @supportURL  https://shapoco.github.io/likespam
+// ==/UserScript==
+
+const profileTextKeys = ['Zy_Iove', 'Su_Iover', 'Zy_iove', 'Xy_Iove', 'ace_GlFT', 'ギフト1万円分'];
+const nameKeys = [
+  '【公式】 いいねされた人はプロフィールをチェックして景品をGET🎁おめでとう🎉',
+];
+
+const screenNameRegex = /^@\w+$/;
+
+var dict = {};
+var ids = [];
+
+const div = document.createElement('div');
+div.style.position = 'fixed';
+div.style.left = '20px';
+div.style.bottom = '20px';
+div.style.width = '200px';
+div.style.height = '150px';
+div.style.background = '#ccc';
+div.innerHTML =
+  '<div id="findspam_status"></div>' +
+  '<button id="findspam_btn">Copy all ids</button>' +
+  '<textarea id="findspan_ids"></textarea>';
+document.getElementsByTagName('body')[0].appendChild(div);
+
+document.addEventListener("scroll", (event) => { clearTimeout(); setTimeout(scanUsers, 1000); });
+
+function scanUsers() {
+  const spans = document.getElementsByTagName('span');
+  var numKnown = 0;
+  var numNormals = 0;
+  //for (var span in spans) {
+  for (var ispan = 0; ispan < spans.length; ispan++) {
+    const span = spans[ispan];
+    const screenName = span.innerHTML;
+    //console.log(screenName);
+    //if (!screenName.startsWith('@')) continue;
+    if (!screenName.match(screenNameRegex)) continue;
+    if (screenName in dict) continue;
+    dict[screenName] = true;
+
+    //const div = span.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.childNodes[2];
+    const div0 = span.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    if (!div0) continue;
+
+    const div0InnerHTML = div0.innerHTML;
+
+    //const nodesSnapshot1 = document.evaluate('div[1]/div[1]/div/div[1]/a/div/div[1]/span]', div0, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    const nodesSnapshot1 = document.evaluate('div[1]/div[1]/div/div[1]/a/div/div[1]/span', div0, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    if (!nodesSnapshot1) continue;
+    //const userName = nodesSnapshot1.snapshotItem(0).innerText;
+    const userName = getInnerTextWithAlt(nodesSnapshot1.snapshotItem(0));
+
+    var containsKey = false;
+
+    for (var ikey = 0; ikey < profileTextKeys.length; ikey++) {
+      if (div0InnerHTML.includes(profileTextKeys[ikey])) {
+        containsKey = true;
+        break;
+      }
+    }
+
+    if (!containsKey) {
+      if (nameKeys.includes(userName)) {
+        containsKey = true;
+      }
+    }
+
+    if (screenName in spamScreenNames) {
+      numKnown += 1;
+      continue;
+    }
+
+    if (!containsKey) {
+      numNormals += 1;
+      continue;
+    }
+
+    const regex0 = /data-testid="(\d+)-(un)?(follow|block)"/;
+    const match0 = div0.innerHTML.match(regex0);
+    const userId = match0[1];
+
+    //const regex1 = /style="text-overflow: unset;">([^<]*)"/;
+    //const regex1 = /(特別なご案内です)/;
+    //const regex1 = /<span[^>]+style="text-overflow: unset;">([^<]+)<\/span>/;
+    //const match1 = div0.innerHTML.match(regex1);
+    //const userName = match1[1];
+
+    //const div1 = div0.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    const div1 = div0.parentElement;
+    //const regex2 = /(https:\/\/pbs.twimg.com\/profile_images\/\d+\/[-\w]+\.\w+)/;
+    //const regex2 = /(https:\/\/pbs.twimg.com\/profile_images\/${userId}\/[-\w]+\.\w+)/;
+    const regex2 = /(https:\/\/pbs.twimg.com\/[^&]+)&quot;/;
+    //const regex2 = /(https:\/\/\w+(\.\w+)+\/profile_images\/\d+\/[-\w]+\.\w+)/;
+    //const regex2 = /(profile_images\/\d+\/[-\w]+\.\w+)/;
+    //const regex2 = /(profile_images[^&]+)&quot;/;
+    //const regex2 = /(profile_images)/;
+    //const regex2 = /(pbs\.twimg\.com)/;
+    const match2 = div1.innerHTML.match(regex2);
+    const profileImage = match2[1];
+    //var profileImage = '';
+    ////if (match2) {
+    ////  profileImage = match2[1];
+    ////}
+    //try {
+    //  profileImage = match2[1];
+    //}
+    //catch {}
+
+    //https://pbs.twimg.com/profile_images/1835709222513971201/O5HOnatY_normal.png
+    const record =
+          getPadRight(userId, 20) + ', ' +
+          getPadRight(screenName.substring(1), 25) + ', ' +
+          getPadRight(toISOStringWithTimezone(new Date()), 25) + ', ' +
+          getPadRight('', 25) + ', ' +
+          userName + ', ' + profileImage;
+    ids.push(record);
+    console.log(record);
+
+    //ids.push(screenName);
+    //console.log(screenName);
+  }
+
+  document.getElementById('findspam_status').innerHTML =
+    ids.length.toString() + ' spams, <br>' +
+    numKnown + ' knowns, <br>' +
+    numNormals + ' normals';
+}
+
+function getInnerTextWithAlt(elm) {
+    if (elm.nodeType === Node.TEXT_NODE) {
+        return elm.nodeValue;
+    } else if (elm.nodeType === Node.ELEMENT_NODE) {
+        if (elm.tagName.toLowerCase() === 'img') {
+            return elm.alt;
+        } else {
+            let text = '';
+            for (let child of elm.childNodes) {
+                text += getInnerTextWithAlt(child);
+            }
+            return text;
+        }
+    }
+    return '';
+}
+
+function getPadRight(s, n) {
+  if (s.length >= n) return s;
+  return s + ' '.repeat(n - s.length);
+}
+
+function toISOStringWithTimezone(date) {
+  const year = date.getFullYear().toString();
+  const month = zeroPadding((date.getMonth() + 1).toString());
+  const day = zeroPadding(date.getDate().toString());
+
+  const hour = zeroPadding(date.getHours().toString());
+  const minute = zeroPadding(date.getMinutes().toString());
+  const second = zeroPadding(date.getSeconds().toString());
+
+  const localDate = `${year}-${month}-${day}`;
+  const localTime = `${hour}:${minute}:${second}`;
+
+  const diffFromUtc = date.getTimezoneOffset();
+
+  // UTCだった場合
+  if (diffFromUtc === 0) {
+    const tzSign = 'Z';
+    return `${localDate}T${localTime}${tzSign}`;
+  }
+
+  // UTCではない場合
+  const tzSign = diffFromUtc < 0 ? '+' : '-';
+  const tzHour = zeroPadding((Math.abs(diffFromUtc) / 60).toString());
+  const tzMinute = zeroPadding((Math.abs(diffFromUtc) % 60).toString());
+
+  return `${localDate}T${localTime}${tzSign}${tzHour}:${tzMinute}`;
+}
+
+function zeroPadding(s) {
+  return ('0' + s).slice(-2);
+}
+
+// //*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[3]/div/div/button/div/div[2]/div[1]/div[1]/div/div[2]/div/a/div/div/span
+//<span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc" style="text-overflow: unset;">@woodcutter_YTs_</span>
+
+// //*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[3]/div/div/button/div/div[2]/div[2]
+
+// //*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[1]/div/div/button/div/div[2]/div[1]/div[1]/div/div[1]/a/div/div[1]/span/span[1]
+// <span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc" style="text-overflow: unset;">【特別なご案内です】 今すぐプロフィ一ル確認して景品GET(キャンペ一ン)へ</span>
+
+// //*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[6]/div/div/button/div/div[2]/div[1]/div[1]/div/div[1]/a/div/div[1]/span
+// <span class="css-1jxf684 r-dnmrzs r-1udh08x r-3s2u2q r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc" style="text-overflow: unset;">
+//   <span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc" style="text-overflow: unset;">【公式】総実績500件突破</span>
+//   <img alt="🔥" draggable="false" src="https://abs-0.twimg.com/emoji/v2/svg/1f525.svg" title="炎" class="r-4qtqp9 r-dflpy8 r-k4bwe5 r-1kpi4qh r-pp5qcn r-h9hxbl">
+//   <span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc" style="text-overflow: unset;">いいねされたらご当選確定！おめでとうございます</span>
+//   <img alt="🎉" draggable="false" src="https://abs-0.twimg.com/emoji/v2/svg/1f389.svg" title="クラッカー" class="r-4qtqp9 r-dflpy8 r-k4bwe5 r-1kpi4qh r-pp5qcn r-h9hxbl">
+//   <span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc r-9iso6" style="text-overflow: unset;"></span>
+// </span>
+
+// //*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[1]/div/div/button/div/div[2]/div[1]/div[1]/div/div[1]/a/div/div[1]/span
+// <span class="css-1jxf684 r-dnmrzs r-1udh08x r-3s2u2q r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc" style="text-overflow: unset;">
+//   <span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc" style="text-overflow: unset;">【公式】総実績500件突破</span>
+//   <img alt="🔥" draggable="false" src="https://abs-0.twimg.com/emoji/v2/svg/1f525.svg" title="炎" class="r-4qtqp9 r-dflpy8 r-k4bwe5 r-1kpi4qh r-pp5qcn r-h9hxbl">
+//   <span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc" style="text-overflow: unset;">いいねされたらご当選確定！おめでとうございます</span>
+//   <img alt="🎉" draggable="false" src="https://abs-0.twimg.com/emoji/v2/svg/1f389.svg" title="クラッカー" class="r-4qtqp9 r-dflpy8 r-k4bwe5 r-1kpi4qh r-pp5qcn r-h9hxbl">
+//   <span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc r-9iso6" style="text-overflow: unset;"></span>
+// </span>
+
+// //*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[2]/div/div/button/div/div[2]/div[1]/div[2]/button
+// <button aria-describedby="id__3vpg9bbvehs" aria-label="フォロー中 @shapoco" role="button" class="css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-15ysp7h r-4wgw6l r-3pj75a r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l" data-testid="858142314849378304-unfollow" type="button" style="background-color: rgba(0, 0, 0, 0); border-color: rgb(207, 217, 222);">
+//   <div dir="ltr" class="css-146c3p1 r-bcqeeo r-qvutc0 r-1tl8opc r-q4m81j r-a023e6 r-rjixqe r-b88u0q r-1awozwy r-6koalj r-18u37iz r-16y2uox r-1777fci" style="text-overflow: unset; color: rgb(15, 20, 25);">
+//     <span class="css-1jxf684 r-dnmrzs r-1udh08x r-3s2u2q r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc r-1b43r93 r-1cwl3u0" style="text-overflow: unset;">
+//       <span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc" style="text-overflow: unset;">
+//         フォロー中
+//       </span>
+//     </span>
+//   </div>
+// </button>
+
+// //*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[1]/div/div/button/div/div[1]/div/div/div[2]/div/div[2]/div/a/div[3]/div/div[2]/div/div
+// <div class="css-175oi2r r-1niwhzg r-vvn4in r-u6sd8q r-1p0dtai r-1pi2tsx r-1d2f490 r-u8s1d r-zchlnj r-ipm5af r-13qz1uu r-1wyyakw r-4gszlv" style="background-image: url(&quot;https://pbs.twimg.com/profile_images/1835709222513971201/O5HOnatY_normal.png&quot;);"></div>
+
+document.getElementById('findspam_btn').addEventListener("click", (event) => {
+  const s = ids.join('\r\n') + '\r\n';
+  document.getElementById('findspan_ids').innerHTML = s;
+  navigator.clipboard.writeText(s);
+});
+//function joinIds() {
+//  //document.getElementById('findspan_ids').innerHTML = ids.join('\r\n');
+//}
+
