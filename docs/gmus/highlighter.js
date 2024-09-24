32d85275a9e42896db70dc24eb9d4053a5a7fee2
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-24 09:49:56 +0900

    update DB

--- docs/gmus/highlighter.js
+++ docs/gmus/highlighter.js
@@ -3,10 +3,10 @@
 // @namespace   https://github.com/shapoco/likespam
 // @match       https://x.com/*
 // @grant       none
-// @version     1.0.145
+// @version     1.0.146
 // @author      Shapoco
 // @description いいねスパムリストに収録済みのユーザーに「収録済」のタグを表示します
-// @require     https://shapoco.github.io/likespam/gmus/db.js?20240924092059
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20240924094956
 // @updateURL   https://shapoco.github.io/likespam/gmus/highlighter.js
 // @downloadURL https://shapoco.github.io/likespam/gmus/highlighter.js
 // @supportURL  https://shapoco.github.io/likespam
@@ -20,17 +20,16 @@ var highlighterTimeoutId = -1;
 highlighterTimeoutId = setTimeout(scanSpams, 1000);
 
 function scanSpams() {
-  scanElems(document.getElementsByTagName('span'));
-  scanElems(document.getElementsByTagName('a'));
+  scanElems(document.getElementsByTagName('span'), '@', spamRegex0);
+  //scanElems(document.getElementsByTagName('a'), 'https://x.com/', spamRegex1);
 }
 
-function scanElems(elems) {
+function scanElems(elems, startMarker, regexp) {
   const n = elems.length;
   for(var i = 0; i < n; i++) {
     const elem = elems[i];
     const innerText = getInnerTextWithAlt(elem);
-    if ((innerText.startsWith('@') && innerText.match(spamRegex0)) ||
-        (innerText.startsWith('https://x.com/') && innerText.match(spamRegex1))) {
+    if (innerText.startsWith(startMarker) && innerText.match(regexp)) {
       //elem.innerHTML += ' <elem style="background: #e00; color: white; border-radius: 999px; font-weight: bold;">&nbsp;収録済&nbsp;</span>';
       elem.style.background = '#fcc';
       elem.style.borderRadius = '5px';
