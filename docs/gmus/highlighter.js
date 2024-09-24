13063469391611a755ce5e00e04a6d5b1dc7b5e0
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-24 09:06:09 +0900

    update DB

--- docs/gmus/highlighter.js
+++ docs/gmus/highlighter.js
@@ -3,10 +3,10 @@
 // @namespace   https://github.com/shapoco/likespam
 // @match       https://x.com/*
 // @grant       none
-// @version     1.0.141
+// @version     1.0.142
 // @author      Shapoco
 // @description いいねスパムリストに収録済みのユーザーに「収録済」のタグを表示します
-// @require     https://shapoco.github.io/likespam/gmus/db.js?20240924085833
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20240924090609
 // @updateURL   https://shapoco.github.io/likespam/gmus/highlighter.js
 // @downloadURL https://shapoco.github.io/likespam/gmus/highlighter.js
 // @supportURL  https://shapoco.github.io/likespam
--- docs/gmus/inforeader.js
+++ docs/gmus/inforeader.js
@@ -3,10 +3,10 @@
 // @namespace   https://github.com/shapoco/likespam
 // @match       https://x.com/search?*
 // @grant       none
-// @version     1.0.4
+// @version     1.0.5
 // @author      Shapoco
 // @description Xの検索結果からスパムアカウントの情報を抽出します
-// @require     https://shapoco.github.io/likespam/gmus/db.js?20240924085833
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20240924090609
 // @updateURL   https://shapoco.github.io/likespam/gmus/inforeader.js
 // @downloadURL https://shapoco.github.io/likespam/gmus/inforeader.js
 // @supportURL  https://shapoco.github.io/likespam
@@ -86,7 +86,7 @@ function scanUsers() {
 
     dict[screenName] = true;
 
-    if (screenName in spamScreenNames) {
+    if (spamScreenNames.includes(screenName.substring(1))) {
       numKnown += 1;
       continue;
     }
