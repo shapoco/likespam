7e66de2020058b84d60aec1e97034f24cea57ccb
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-24 09:40:15 +0900

    update script

--- docs/gmus/highlighter.js
+++ docs/gmus/highlighter.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/shapoco/likespam
 // @match       https://x.com/*
 // @grant       none
-// @version     1.0.144
+// @version     1.0.145
 // @author      Shapoco
 // @description いいねスパムリストに収録済みのユーザーに「収録済」のタグを表示します
 // @require     https://shapoco.github.io/likespam/gmus/db.js?20240924092038
