c6521d21ebe80a653d271ac6d378be64f15b81b0
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-27 09:51:38 +0900

    update DB

--- docs/gmus/inforeader.js
+++ docs/gmus/inforeader.js
@@ -3,10 +3,10 @@
 // @namespace   https://github.com/shapoco/likespam
 // @match       https://x.com/search?*
 // @grant       none
-// @version     1.0.227
+// @version     1.0.228
 // @author      Shapoco
 // @description Xの検索結果からスパムアカウントの情報を抽出します
-// @require     https://shapoco.github.io/likespam/gmus/db.js?20240927033424
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20240927095138
 // @updateURL   https://shapoco.github.io/likespam/gmus/inforeader.js
 // @downloadURL https://shapoco.github.io/likespam/gmus/inforeader.js
 // @supportURL  https://shapoco.github.io/likespam
--- docs/index.js
+++ docs/index.js
@@ -70,7 +70,7 @@ function likeSpamRenderStats() {
     '</table>';
 }
 
-function getSearchLinkHtml(screenNames, linkText) {
+function likeSpamGetSearchLinkHtml(screenNames, linkText) {
   const url = `https://x.com/search?q=${screenNames.map(e => '%40' + e).join('+OR+')}&src=typed_query&f=user`;
   if (!linkText || linkText.length == 0) {
     linkText = screenNames.map(e => '@' + e).join(' ');
@@ -78,3 +78,40 @@ function getSearchLinkHtml(screenNames, linkText) {
   return `<a href="${url}" target="_blank">${linkText}</a>`;
 }
 
+function likeSpamFormatDate(d) {
+  const now = new Date();
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const tommorow = new Date(today);
+  tommorow.setDate(tommorow.getDate() + 1);
+  const yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  const isToday = today <= d && d < tommorow;
+  const isYesterday = d >= yesterday && d < today;
+  const isThisYear = d.getFullYear() === now.getFullYear();
+
+  const hours = d.getHours().toString();
+  const minutes = d.getMinutes().toString().padStart(2, '0');
+
+  if (isToday) {
+    return `今日 ${hours}:${minutes}`;
+  }
+  else if (isYesterday) {
+    return `昨日 ${hours}:${minutes}`;
+  }
+  else if (isThisYear) {
+    const month = (d.getMonth() + 1);
+    const day = d.getDate().toString().padStart(2, '0');
+    return `${month}/${day} ${hours}:${minutes}`;
+  }
+  else {
+    const year = d.getFullYear().toString().slice(-2);
+    const month = (d.getMonth() + 1);
+    const day = d.getDate().toString().padStart(2, '0');
+    return `${year}/${month}/${day} ${hours}:${minutes}`;
+  }
+}
+
+function isSmartPhone() {
+  return window.matchMedia && window.matchMedia('(max-device-width: 640px)').matches;
+}
