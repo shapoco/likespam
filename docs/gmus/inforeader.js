9a3ea45b49ce7a81b5df3eb5c90bafba3f49c5bf
shapoco <40152459+shapoco@users.noreply.github.com>
2024-09-29 11:23:55 +0900

    update DB

--- docs/gmus/inforeader.js
+++ docs/gmus/inforeader.js
@@ -3,10 +3,10 @@
 // @namespace   https://github.com/shapoco/likespam
 // @match       https://x.com/search?*
 // @grant       none
-// @version     1.0.335
+// @version     1.0.336
 // @author      Shapoco
 // @description Xの検索結果からスパムアカウントの情報を抽出します
-// @require     https://shapoco.github.io/likespam/gmus/db.js?20240929112207
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20240929112355
 // @updateURL   https://shapoco.github.io/likespam/gmus/inforeader.js
 // @downloadURL https://shapoco.github.io/likespam/gmus/inforeader.js
 // @supportURL  https://shapoco.github.io/likespam
