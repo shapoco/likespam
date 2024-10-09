fb46701511422013759b58cafc18b20627d3d936
shapoco <40152459+shapoco@users.noreply.github.com>
2024-10-09 16:59:12 +0900

    screen name of Zy_Iove is changed to OfficiaI_______

--- docs/gmus/highlighter.js
+++ docs/gmus/highlighter.js
@@ -4,10 +4,10 @@
 // @match       https://x.com/*
 // @match       https://pro.twitter.com/*
 // @grant       none
-// @version     1.0.840
+// @version     1.0.841
 // @author      Shapoco
 // @description いいねスパムリストに収録済みのユーザーを強調表示します。
-// @require     https://shapoco.github.io/likespam/gmus/db.js?20241009012716
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20241009165853
 // @updateURL   https://shapoco.github.io/likespam/gmus/highlighter.js
 // @downloadURL https://shapoco.github.io/likespam/gmus/highlighter.js
 // @supportURL  https://shapoco.github.io/likespam
