b30775522aa7e021e0a3ed32dfbf8ff52d26ec64
shapoco <40152459+shapoco@users.noreply.github.com>
2024-10-09 20:17:42 +0900

    update DB

--- docs/gmus/highlighter.js
+++ docs/gmus/highlighter.js
@@ -4,10 +4,10 @@
 // @match       https://x.com/*
 // @match       https://pro.twitter.com/*
 // @grant       none
-// @version     1.0.851
+// @version     1.0.852
 // @author      Shapoco
 // @description いいねスパムリストに収録済みのユーザーを強調表示します。
-// @require     https://shapoco.github.io/likespam/gmus/db.js?20241009195619
+// @require     https://shapoco.github.io/likespam/gmus/db.js?20241009201742
 // @updateURL   https://shapoco.github.io/likespam/gmus/highlighter.js
 // @downloadURL https://shapoco.github.io/likespam/gmus/highlighter.js
 // @supportURL  https://shapoco.github.io/likespam
